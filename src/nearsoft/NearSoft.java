c0bb0705bfb27ea2cc776164a35c7ed6b3d7e8ad
Jofiel Batista Farias <jofielbatista@hotmail.com>
2018-08-24 23:25:10 -0600

    Segundo Cambio Del Proyecto "Contador"

--- src/nearsoft/NearSoft.java
+++ src/nearsoft/NearSoft.java
@@ -2,6 +2,11 @@ package nearsoft;
 
 public class NearSoft {
     public static void main(String[] args) {
-        System.out.println("Listo");
+        System.out.println("Incio de contador");
+        int contador = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(contador);
+            contador++;
+        }
     }   
 }
